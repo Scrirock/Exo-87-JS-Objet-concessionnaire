dc806f7a8a1c896c12c5480fb41b9744af46fb51
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-12-15 15:51:32 +0100

    fcyhu

--- exercice1.js
+++ exercice1.js
@@ -1,15 +1,24 @@
 let voiture = {
-    "Nom" : "Eggo",
-    "imgUrl" : "https://images.caradisiac.com/logos/9/9/8/2/159982/S7-Citroen-Eggo-concept-de-citadine-du-futur-73689.jpg",
-    "nombresRoues" : 4,
-    "Couleur" : "Marron",
-    "Contructeur" : "Citroen",
-    "Carburant" : "Solaire",
-    "nombrePortes" : 3,
-    "Autonomie" : "450km",
-    "vitesseMaxi" : "120km/h"
+    nom : "Eggo",
+    imgUrl : "https://images.caradisiac.com/logos/9/9/8/2/159982/S7-Citroen-Eggo-concept-de-citadine-du-futur-73689.jpg",
+    nombresRoues : 4,
+    couleur : "Marron",
+    contructeur : "Citroen",
+    carburant : "Solaire",
+    nombrePortes : 3,
+    autonomie : "450km",
+    vitesseMaxi : "120km/h"
 };
 
-
+document.getElementById("image").src = voiture.imgUrl;
+document.getElementById("image").style.float = "right";
+document.getElementById("nom").innerHTML = "Nom: " + voiture.nom;
+document.getElementById("nbrPorte").innerHTML = "Nombre de porte: " + voiture.nombrePortes;
+document.getElementById("nbrRoues").innerHTML = "Nombre de roues: " + voiture.nombresRoues;
+document.getElementById("couleur").innerHTML = "Couleur: " + voiture.couleur;
+document.getElementById("carburant").innerHTML = "Carburant utilisé: " + voiture.carburant;
+document.getElementById("constructeur").innerHTML = "Constructeur: " + voiture.contructeur;
+document.getElementById("autonomie").innerHTML = "Autonomie: " + voiture.autonomie;
+document.getElementById("vitesseMax").innerHTML = "Vitesse maximum: " + voiture.vitesseMaxi;
 
 
